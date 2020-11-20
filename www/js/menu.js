214d046168a2d32ff1f3aebad003081cf8ecdb10
dwarkesh8 <dwarkeshpurohit8@gmail.com>
2020-11-20 16:57:58 +0530

    changed menu page, added side navbar

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,3 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 $(document).ready(function(){
 	$("#loading").css('display','none');
 	$("#mainDiv").css('visibility','visible');
