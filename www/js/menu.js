d06945a5588f33cf134c9eec20a8dc2acd2bb39d
dwarkesh8 <dwarkeshpurohit8@gmail.com>
2020-11-21 11:46:58 +0530

    fixed sidenav bar width

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "200px";
+  document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("mainDiv").style.filter = "blur(8px)";
 }
 
